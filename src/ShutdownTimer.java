1a68c37282dc31abc8e5df80998652d949585db1
Tobias Hardt <tobiashardt@t-online.de>
2020-11-26 20:17:38 +0100

    Adds support for own additional parameters

--- src/ShutdownTimer.java
+++ src/ShutdownTimer.java
@@ -1,20 +1,28 @@
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ShutdownTimer extends JFrame {
 
-    private JButton applyButton = new JButton("Timer starten");
-    private JButton cancelButton = new JButton("Herunterfahren abbrechen");
-    private JSpinner minuteSpinner = new JSpinner();
-    private ButtonGroup stateGroup = new ButtonGroup();
-    private JRadioButton radioShutdown = new JRadioButton();
-    private JRadioButton radioRestart = new JRadioButton();
-    private JCheckBox notificationsCheckbox = new JCheckBox("Benachrichtigungen ausblenden");
-    private JCheckBox forceShutdownCheckbox = new JCheckBox("Herunterfahren erzwingen");
-    private JComboBox timeUnitComboBox = new JComboBox();
+    private final JButton applyButton = new JButton("Timer starten");
+    private final JButton cancelButton = new JButton("Herunterfahren abbrechen");
+    private final JButton helpButton = new JButton("?");
+    private final JSpinner minuteSpinner = new JSpinner();
+    private final ButtonGroup stateGroup = new ButtonGroup();
+    private final JRadioButton radioShutdown = new JRadioButton();
+    private final JRadioButton radioRestart = new JRadioButton();
+    private final JCheckBox notificationsCheckbox = new JCheckBox("Benachrichtigungen ausblenden");
+    private final JCheckBox forceShutdownCheckbox = new JCheckBox("Herunterfahren erzwingen");
+    private final JComboBox<String> timeUnitComboBox = new JComboBox<>();
+    private final String parameterPlaceholder = "Eigene Parameter";
+    private final JTextField parameterTextField = new JTextField(parameterPlaceholder);
 
     public ShutdownTimer(String name) {
         super(name);
@@ -24,11 +32,7 @@ public class ShutdownTimer extends JFrame {
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+        javax.swing.SwingUtilities.invokeLater(ShutdownTimer::createAndShowGUI);
     }
 
     private static void createAndShowGUI() {
@@ -58,7 +62,7 @@ public class ShutdownTimer extends JFrame {
 
         // Param panel for additional parameters
         final JPanel paramPanel = new JPanel();
-        GridLayout paramLayout = new GridLayout(3, 1);
+        GridBagLayout paramLayout = new GridBagLayout();
         paramPanel.setLayout(paramLayout);
 
         // Control panel for buttons
@@ -68,9 +72,9 @@ public class ShutdownTimer extends JFrame {
         controlPanel.setLayout(controlLayout);
 
         // Create combobox with time units
-        String timeUnits[] = {"Sekunden", "Minuten", "Stunden"};
-        DefaultComboBoxModel comboBoxModel = new DefaultComboBoxModel(timeUnits);
-        timeUnitComboBox.setLightWeightPopupEnabled (false);
+        String[] timeUnits = {"Sekunden", "Minuten", "Stunden"};
+        DefaultComboBoxModel<String> comboBoxModel = new DefaultComboBoxModel<>(timeUnits);
+        timeUnitComboBox.setLightWeightPopupEnabled(false);
         timeUnitComboBox.setModel(comboBoxModel);
         timeUnitComboBox.setSelectedIndex(1);
 
@@ -91,51 +95,103 @@ public class ShutdownTimer extends JFrame {
         actionPanel.add(radioShutdown);
         actionPanel.add(radioRestart);
 
-        // Add checkboxes
-        paramPanel.add(new Label("Zusätzliche Parameter:"));
+        // Add checkboxes to GridBagLayout
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 100;
+        gbc.fill = GridBagConstraints.BOTH;
+        paramPanel.add(new Label("Zusätzliche Parameter:"), gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 10;
         notificationsCheckbox.setSelected(true);
-        paramPanel.add(notificationsCheckbox);
+        paramPanel.add(notificationsCheckbox, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 10;
         forceShutdownCheckbox.setSelected(true);
-        paramPanel.add(forceShutdownCheckbox);
+        paramPanel.add(forceShutdownCheckbox, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.weightx = 9;
+        parameterTextField.setForeground(Color.GRAY);
+        paramPanel.add(parameterTextField, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.weightx = 1;
+        paramPanel.add(helpButton, gbc);
+
+        // Listener for parameter text field to show placeholder if empty
+        parameterTextField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (parameterTextField.getText().equals(parameterPlaceholder)) {
+                    parameterTextField.setText("");
+                    parameterTextField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (parameterTextField.getText().isEmpty()) {
+                    parameterTextField.setForeground(Color.GRAY);
+                    parameterTextField.setText(parameterPlaceholder);
+                }
+            }
+        });
+        parameterTextField.requestFocus();
+
         // Add apply and cancel buttons
         controlPanel.add(applyButton);
         controlPanel.add(cancelButton);
 
         // Process the apply buttons functionality and start the timer
-        applyButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Process p = null;
-                try {
-                    p = Runtime.getRuntime().exec(
-                            "cmd /c shutdown " + getSelectedOptions());
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+        applyButton.addActionListener(e -> {
+            try {
+                Runtime.getRuntime().exec("cmd /c shutdown " + getSelectedOptions());
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
         });
 
         // Process the cancel buttons functionality and stop the shutdown
-        cancelButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Process p = null;
-                try {
-                    p = Runtime.getRuntime().exec(
-                            "cmd /c shutdown /a");
+        cancelButton.addActionListener(e -> {
+            try {
+                Runtime.getRuntime().exec("cmd /c shutdown -a");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
 
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+        // Process the help buttons functionality and show shutdown help
+        helpButton.addActionListener(e -> {
+            try {
+                final Process p = Runtime.getRuntime().exec("cmd /c shutdown -?");
+                final ProcessResultReader stdout = new ProcessResultReader(p.getInputStream(), "STDOUT");
+                stdout.start();
+                p.waitFor();
+                JTextArea jTextArea = new JTextArea(stdout.toString());
+                jTextArea.setLineWrap(true);
+                jTextArea.setWrapStyleWord(true);
+                jTextArea.setEditable(false);
+                JScrollPane scrollPane = new JScrollPane(jTextArea);
+                scrollPane.setPreferredSize(new Dimension(500, 500));
+                JOptionPane.showMessageDialog(null, scrollPane);
+                System.out.println(stdout.toString());
+            } catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
             }
         });
 
         JSeparator separator1 = new JSeparator();
-        separator1.setPreferredSize(new Dimension(1,1));
+        separator1.setPreferredSize(new Dimension(1, 1));
 
         JSeparator separator2 = new JSeparator();
-        separator2.setPreferredSize(new Dimension(1,1));
+        separator2.setPreferredSize(new Dimension(1, 1));
 
         JSeparator separator3 = new JSeparator();
-        separator3.setPreferredSize(new Dimension(1,1));
+        separator3.setPreferredSize(new Dimension(1, 1));
 
         pane.add(inputPanel);
         pane.add(actionPanel);
@@ -146,7 +202,7 @@ public class ShutdownTimer extends JFrame {
     }
 
     private String getSelectedOptions() {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         if (radioShutdown.isSelected()) {
             sb.append("-s");
@@ -160,6 +216,10 @@ public class ShutdownTimer extends JFrame {
 
         if (notificationsCheckbox.isSelected()) {
             sb.append(" -c \" \"");
+        }
+
+        if (!parameterTextField.getText().equals(parameterPlaceholder)) {
+            sb.append(" ").append(parameterTextField.getText());
         }
 
         int timeFactor = 1;
@@ -176,8 +236,40 @@ public class ShutdownTimer extends JFrame {
         }
 
         int inputInSeconds = (int) minuteSpinner.getValue() * timeFactor;
-        sb.append(" -t " + inputInSeconds);
+        sb.append(" -t ").append(inputInSeconds);
 
         return sb.toString();
+    }
+
+    private static class ProcessResultReader extends Thread {
+        final InputStream is;
+        final String type;
+        final StringBuilder sb;
+
+        ProcessResultReader(final InputStream is, String type) {
+            this.is = is;
+            this.type = type;
+            this.sb = new StringBuilder();
+        }
+
+        public void run() {
+            try {
+                final InputStreamReader isr = new InputStreamReader(is, "IBM850");
+                final BufferedReader br = new BufferedReader(isr);
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    this.sb.append(line).append("\n");
+                    System.out.println(line);
+                }
+            } catch (final IOException ioe) {
+                System.err.println(ioe.getMessage());
+                throw new RuntimeException(ioe);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return this.sb.toString();
+        }
     }
 }
